0f9459687b03d25ce6c20a78b5d3512f287aa8a6
bandar saud <bandar.programer@gmail.com>
2022-08-05 23:29:51 +0300

    Add convertDate method

--- src/bandar/DateAndTime/WorkWithDateAndTime.java
+++ src/bandar/DateAndTime/WorkWithDateAndTime.java
@@ -1,11 +1,13 @@
 package bandar.DateAndTime;
 
+import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.Date;
 
 public class WorkWithDateAndTime {
 
@@ -18,5 +20,27 @@ public class WorkWithDateAndTime {
 		
 		//ZoneId.getAvailableZoneIds().forEach(System.out::println);
 		System.out.println(LDT);
+		
+		final String FROM = "yyyy-MM-dd";
+		final String TO = "dd.MM.yyyy";
+		final String INPUT = "2022-01-01";
+		
+		System.out.println(convertDate(INPUT, FROM, TO));
+	}
+	
+	public static String convertDate(String input, String from, String to) {
+		String output = null;
+		
+		try {
+			SimpleDateFormat fromFormat = new SimpleDateFormat(from);
+			SimpleDateFormat toFormat = new SimpleDateFormat(to);
+			
+			Date date = fromFormat.parse(input);
+			output = toFormat.format(date);
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return output ;
 	}
 }
